3b762336617e7828677b31b557e910875e6f0ea1
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-04-18 15:44:07 +0530

    Update settings.py

--- HclsPro/HclsPro/settings.py
+++ HclsPro/HclsPro/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
         'NAME': os.environ.get('DB_NAME', 'hclsdb'),
         'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'Vineeth@95'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '3306'),
     }
